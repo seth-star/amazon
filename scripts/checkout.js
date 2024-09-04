54636ad7dcde923cc8bb8e0b65115524c0063cc1
Shango <shangozonveni@gmail.com>
2024-09-04 10:44:06 +0200

    Loading checkout using promise.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,10 +3,20 @@ import { renderPaymentSummary } from "./checkout/paymentsummary.js";
 //import '../data/cart-class.js';
 import { loadProducts } from "../data/products.js";
 
-
-
+new Promise((resolve)=>{
 loadProducts(()=>{
+  resolve();
+})
+}).then(()=>{
+  return new Promise((resolve)=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+    resolve()
+  })
+})
+
+/*loadProducts(()=>{
   renderOrderSummary();
   renderPaymentSummary();
 })
-
+*/
